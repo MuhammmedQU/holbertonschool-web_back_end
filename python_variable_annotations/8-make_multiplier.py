86a1c2e9343e4f9d922b6204d7b8d97d99c28a11
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-04-07 16:19:00 +0400

    multiplier

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module for to_kv function"""
+
+
+from typing import Callable
+
+def make_multiplier(multiplier: float) ->Callable[[float], float]:
+    """Returns a function that multiplies a float by multiplier"""
+    def multiplier_func(value: float) -> float:
+        """Multiplies value by multiplier"""
+        return value * multiplier
+
+    return multiplier_func    
