e4bccdaf7b36c8e5c981dec1cfee456acbef4595
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-04-07 15:37:32 +0400

    floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module for floor function"""
+
+
+from sympy import floor
+
+
+def floor_func(a: float) -> int:
+    """Return the floor of a float"""
+    return floor(a)
