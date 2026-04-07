69e048ce534e9f3eaadcf36d778b68ca34f47f37
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-04-07 17:00:16 +0400

    async0

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Async Comprehension"""
+
+import asyncio
+import random
+
+async def wait_random(max_delay=10):
+    """Wait for a random delay between 0 and max_delay seconds"""
+    time=random.uniform(0, max_delay)
+    await asyncio.sleep(time)
+    return time
