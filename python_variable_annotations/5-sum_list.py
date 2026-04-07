448af0b35cf6d5143da91803a4e85122e5a46785
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-04-07 15:48:27 +0400

    sumlsit

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-"""Module for to_str function"""
+"""Module for sum_list function"""
 
 
-def sum_list(input_list:list[float]) -> float:
+def sum_list(input_list: list[float]) -> float:
     """Return the sum of a list of floats"""
     return sum(input_list)
