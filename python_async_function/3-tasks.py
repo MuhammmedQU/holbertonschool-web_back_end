cc5e51982361d747cd74bc9b749364425da95dc4
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-04-07 17:57:39 +0400

    async3

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,25 +1,29 @@
 #!/usr/bin/env python3
 """Module for task wait generator."""
 
-
 import asyncio
 import importlib
 from typing import AsyncGenerator
 
-wait_random = importlib.import_module("1-concurrent_coroutines").wait_random
+wait_random = importlib.import_module("0-basic_async_syntax").wait_random
 
 
 async def task_wait_random(max_delay: int) -> AsyncGenerator[float, None]:
-    """Return an async generator that yields a random delay"""
+    """Return an async generator that yields a random delay."""
     delay = await wait_random(max_delay)
     yield delay
 
 
 async def task_wait_n(n: int, max_delay: int) -> list[float]:
-    """Return a list of delays from n random wait tasks"""
-    tasks = [task_wait_random(max_delay) for _ in range(n)]
-    delays = []
+    """Return a list of delays from n random wait tasks."""
+    tasks = [
+        asyncio.create_task(task_wait_random(max_delay).__anext__())
+        for _ in range(n)
+    ]
+
+    delays: list[float] = []
     for task in asyncio.as_completed(tasks):
-        delay = await task.__anext__()
+        delay = await task
         delays.append(delay)
+
     return delays
