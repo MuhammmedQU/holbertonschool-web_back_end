2f1a8521dc43491048061759c22c8f5525ffdada
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-04-07 15:56:18 +0400

    summixedlist

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -2,8 +2,9 @@
 """Module for sum_list function"""
 
 from typing import List, Union
+import typing
 
 
-def sum_mixed_list(mxt_list: Union[List[float], List[int]]) -> float:
+def sum_mixed_list(mxt_list: Union[typing.List[float], typing.List[int]]) -> float:
     """Return the sum of a list of floats"""
     return sum(mxt_list)
