ffdb952f0419c6684d156fde935907ca30883fd4
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-04-09 02:44:25 +0400

    task0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """Async Generator that loops 10 times"""
 
-
 import asyncio
 import random
 
-async def main():
+
+async def async_generator():
     """Async Generator that loops 10 times"""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0,10)
+        yield random.uniform(0, 10)
