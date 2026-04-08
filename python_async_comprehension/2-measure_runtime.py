c7573bcff7969cbaf2e34f59a5b4fc06edaf6e8a
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-04-09 02:47:34 +0400

    task2

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Measure runtime for four parallel async comprehensions"""
+
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    """Execute async_comprehension four times in parallel and measure runtime"""
+    start = time.perf_counter()
+
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+
+    end = time.perf_counter()
+    return end - start
