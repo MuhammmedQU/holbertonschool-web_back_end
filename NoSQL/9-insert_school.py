f501fa7146169b826dae6787e3bbe0f1b20ebcc0
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-05-05 18:15:33 +0400

    NoSQL9

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -2,6 +2,7 @@
 """Module that lists all documents in a collection"""
 
 
-def update_topics(mongo_collection, name, topics):
-    """Update topics for a school"""
-    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
+def insert_school(mongo_collection, **kwargs):
+    """Insert a new school document into the collection."""
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
