cf122941affc27419481d832c59d2dc75318e415
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-05-05 19:04:15 +0400

    NoSQL12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -6,16 +6,11 @@ from pymongo import MongoClient
 client = MongoClient("mongodb://127.0.0.1:27017")
 collection = client.logs.nginx
 
-# total logs
 print(f"{collection.count_documents({})} logs")
-
-# methods
-print("Methods:")
 methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 for m in methods:
     count = collection.count_documents({"method": m})
     print(f"\tmethod {m}: {count}")
 
-# status check
 count = collection.count_documents({"method": "GET", "path": "/status"})
 print(f"{count} status check")
