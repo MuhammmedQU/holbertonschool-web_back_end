0ab9cd243bb7e43fe38c380f5b9fa55aa171640b
MuhammmedQU <ahmedov.muhammed.16112006@gmail.com>
2026-05-16 20:11:22 +0400

    Update 8-clean_set.js

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,24 @@
 const cleanSet = (set, startString) => {
   const array = [];
-  if (!startString) {
-    return ``;
+
+  if (typeof startString !== 'string' || startString === '') {
+    return '';
   }
+
   for (const element of set) {
-    if (typeof element === 'string' && element.startsWith(startString)) {
-      array.push(element.slice(startString.length));
+
+    if (
+      typeof element === 'string' &&
+      element.startsWith(startString)
+    ) {
+
+      array.push(
+        element.slice(startString.length)
+      );
+
     }
   }
+
   return array.join('-');
 }
 
