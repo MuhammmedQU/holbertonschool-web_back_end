d1db1eeea4b71bfc68e2f3046796394b6a2cd561
MuhammmedQU <ahmedov.muhammed.16112006@gmail.com>
2026-05-16 19:52:10 +0400

    Create 8-clean_set.js

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,14 @@
+const cleanSet = (set, startString) => {
+  const array = [];
+  if (startString === ``) {
+    return ``;
+  }
+  for (const element of set) {
+    if (element.startsWith(startString)) {
+      array.push(element.slice(startString.length));
+    }
+  }
+  return array.join('-');
+}
+
+export default cleanSet;
