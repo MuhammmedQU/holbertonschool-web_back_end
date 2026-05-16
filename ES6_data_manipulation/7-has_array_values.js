156998be1243e3357968b7cf8e031998c1858676
MuhammmedQU <ahmedov.muhammed.16112006@gmail.com>
2026-05-16 19:43:15 +0400

    Create 7-has_array_values.js

---
+++ ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,11 @@
+const hasValuesFromArray = (set,array) =>{ 
+
+  for (let i = 0; i < array.length; i++) {
+    if (!set.has(array[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+export default hasValuesFromArray;
